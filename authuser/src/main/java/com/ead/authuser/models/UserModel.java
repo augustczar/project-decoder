714b08c27946d81bb1929a96a74ec07b336dd6a9
augustczar <augustczarramos@gmail.com>
2023-08-16 11:18:57 -0300

    Creation Entities model  and Enums

---
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,65 @@
+package com.ead.authuser.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+
+	private static final long serialVersionUID = 969341758235417006L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID userId;
+	
+	@Column(nullable = false, unique = true, length = 50)
+	private String userName;
+	
+	@Column(nullable = false, unique = true, length = 50)
+	private String email;
+	
+	@Column(nullable = false, length = 255)
+	private String password;
+	
+	@Column(nullable = false, length = 150)
+	private String fullName;
+	
+	@Enumerated(EnumType.STRING)
+	private UserStatus userStatus;
+	
+	@Enumerated(EnumType.STRING)
+	private UserType userType;
+	
+	@Column(length = 20)
+	private String phoneNumber;
+	
+	@Column(length = 20)
+	private String cpf;
+	
+	@Column
+	private String imageUrl;
+	
+	@Column(nullable = false)
+	private LocalDateTime creationDate;
+	
+	@Column(nullable = false)
+	private LocalDateTime lastUpdateDate;
+	
+}
