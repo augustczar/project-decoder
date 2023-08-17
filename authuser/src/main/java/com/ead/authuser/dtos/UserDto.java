689cafca8688fdbf05839ae2d8e905e0ba101f27
augustczar <augustczarramos@gmail.com>
2023-08-17 12:27:32 -0300

    Creation of jsonView in its attributes and interface implementation

--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -4,29 +4,52 @@ import java.util.Objects;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
+	
+	/**
+	 * Criacao do jsonView em seus atributos e 
+	 * implementacao de interface
+	 * para diferentes visoes do usuario no registro e na
+	 * atualizacao dos dados sem a nescecidade de criacao 
+	 * de mais dtos para updade na data base
+	 */
+	public interface UserView{
+		public static interface RegistrationPost{}
+		public static interface UserPut{}
+		public static interface PasswordPut{}
+		public static interface ImagePut{}
+	}
 
 	private UUID userId;
-	
+
+	@JsonView(UserView.RegistrationPost.class)
 	private String userName;
 	
+	@JsonView(UserView.RegistrationPost.class)
 	private String email;
 	
+	@JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	private String password;
 	
+	@JsonView(UserView.PasswordPut.class)
 	private String oldPassword; 
 	
+	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
 	private String fullName;
 	
+	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
 	private String phoneNumber;
 	
+	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
 	private String cpf;
 	
+	@JsonView(UserView.ImagePut.class)
 	private String imageUrl;
 
 	public UserDto() {
