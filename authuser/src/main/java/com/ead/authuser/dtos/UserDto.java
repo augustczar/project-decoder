6e7a1525d1e8d7c3441a7c1d57c9e705ef7dee9f
augustczar <augustczarramos@gmail.com>
2023-08-30 11:24:40 -0300

    Add the Spring validation

--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -3,6 +3,10 @@ package com.ead.authuser.dtos;
 import java.util.Objects;
 import java.util.UUID;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -28,15 +32,24 @@ public class UserDto {
 
 	private UUID userId;
 
+	@NotBlank(groups = UserView.RegistrationPost.class)
+	@Size(min = 4, max = 50)
 	@JsonView(UserView.RegistrationPost.class)
 	private String userName;
 	
+	@NotBlank(groups = UserView.RegistrationPost.class)
+	@Size(min = 4, max = 50)
+	@Email
 	@JsonView(UserView.RegistrationPost.class)
 	private String email;
 	
+	@NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+	@Size(min = 6, max = 20)
 	@JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	private String password;
 	
+	@NotBlank(groups = UserView.PasswordPut.class)
+	@Size(min = 6, max = 20)
 	@JsonView(UserView.PasswordPut.class)
 	private String oldPassword; 
 	
@@ -49,24 +62,8 @@ public class UserDto {
 	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
 	private String cpf;
 	
+	@NotBlank(groups = UserView.ImagePut.class)
 	@JsonView(UserView.ImagePut.class)
 	private String imageUrl;
-
-	public UserDto() {
-	}
-
-	public UserDto(UUID userId, String userName, String email, String password, String oldPassword, String fullName,
-			String phoneNumber, String cpf, String imageUrl) {
-		super();
-		this.userId = userId;
-		this.userName = userName;
-		this.email = email;
-		this.password = password;
-		this.oldPassword = oldPassword;
-		this.fullName = fullName;
-		this.phoneNumber = phoneNumber;
-		this.cpf = cpf;
-		this.imageUrl = imageUrl;
-	}
 
 }
