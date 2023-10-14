230a908ccfbf4567ebd6d5d69527ec3a275b7c7f
augustczar <augustczarramos@gmail.com>
2023-10-14 19:31:13 -0300

    API Composition Pattern - Prepare GetAll Methods and Pattern in Authuser Microservice – Part 01 and Part 2

--- course/src/main/java/com/ead/course/dtos/CourseDto.java
+++ course/src/main/java/com/ead/course/dtos/CourseDto.java
@@ -8,25 +8,35 @@ import javax.validation.constraints.NotNull;
 import com.ead.course.enums.CourseLevel;
 import com.ead.course.enums.CourseStatus;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class CourseDto {
 
 	@NotBlank
 	private String name;
-	
+
 	@NotBlank
 	private String description;
 	
 	private String imageUrl;
-	
+
 	@NotNull
 	private CourseStatus courseStatus;
-	
+
 	@NotNull
 	private UUID userInstructor;
-	
+
 	@NotNull
 	private CourseLevel courseLevel;
 }
