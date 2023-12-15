81e776ee3ad5ba40b8311bb3823c140e511b43b3
augustczar <augustczarramos@gmail.com>
2023-12-15 15:37:05 -0300

    Create User Instructor Type to Create Courses

---
+++ authuser/src/main/java/com/ead/authuser/dtos/InstructorDto.java
@@ -0,0 +1,24 @@
+package com.ead.authuser.dtos;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class InstructorDto {
+
+	@NotNull
+	private UUID userId;
+}
--- authuser/src/main/java/com/ead/authuser/enums/UserType.java
+++ authuser/src/main/java/com/ead/authuser/enums/UserType.java
@@ -4,5 +4,5 @@ public enum UserType {
 
 	ADMIN,
 	STUDENT,
-	STRUCTOR;
+	INSTRUCTOR;
 }
