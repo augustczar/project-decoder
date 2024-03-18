382098588c4fde3c6b5126f0fd94e497165cebcd
augustczar <augustczarramos@gmail.com>
2024-03-18 15:58:32 -0300

    Update Config Server With Spring Cloud Config and Git repository

--- config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
@@ -1,13 +1,13 @@
 package com.ead.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
