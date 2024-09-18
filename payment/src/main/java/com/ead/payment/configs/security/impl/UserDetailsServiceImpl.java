f95d3ba246cfe58dd731da774a0ebedc27e56f38
augustczar <augustczarramos@gmail.com>
2024-09-18 13:03:57 -0300

    Payment Microservice service created

---
+++ payment/src/main/java/com/ead/payment/configs/security/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ead.payment.configs.security.impl;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl{
+
+    public UserDetailsImpl build(UUID userId, String rolesStr) {
+        return UserDetailsImpl.build(userId, rolesStr);
+    }
+}
